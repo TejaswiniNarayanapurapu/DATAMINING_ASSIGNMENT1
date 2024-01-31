0b6214c0d1372797441307ffecafc194a65d9feb
TejaswiniNarayanapurapu <158120178+TejaswiniNarayanapurapu@users.noreply.github.com>
2024-01-30 22:55:02 -0500

    Add files via upload

--- question7.py
+++ question7.py
@@ -1,14 +1,14 @@
 def question7_1():
-    answer ='increase/decrease'
-    return answer 
+    answer = 'increase/decrease'
+    return answer
 
 def question7_2():
-    answer = 'non-decreasing'
+    answer = 'non-increasing'
     return answer
 
 def question7_3():
     answer = [(0., 4.), 
               (4., 5.), 
               (5., 8.), 
-              (8., 'infinity')]    
+              (8., 'infinity')]
     return answer
